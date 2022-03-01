5ae6048f9e04b0e611a0d0a880aeeb32df3c23f8
LF56 <leah.fox7@gmail.com>
2022-03-01 12:51:05 -0500

    changed variable

--- data/server.js
+++ data/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { animals } = require('./data/animals');
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 
@@ -9,6 +10,7 @@ const app = express();
 app.get('/api/animals', (req, res) => {
     res.send('Hello!');
   });
-app.listen(3001, () => {
-    console.log(`API server now on port 3001`)
+  app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+  });
 });
