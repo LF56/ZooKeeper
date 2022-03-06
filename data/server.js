41f2d2659095584ab825b0c0883a2c5669a5eb13
LF56 <leah.fox7@gmail.com>
2022-03-06 13:51:49 -0500

    final additions to code

--- data/server.js
+++ data/server.js
@@ -1,16 +1,18 @@
 const express = require('express');
-const { animals } = require('./data/animals');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
 
+// Use apiRoutes
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
-
-
-app.get('/api/animals', (req, res) => {
-    res.send('Hello!');
-  });
-  app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
-  });
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
 });
